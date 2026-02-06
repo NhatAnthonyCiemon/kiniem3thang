5af5f38250e5e48db005fec7cbb834d607eb3ca9
TNMonster <nhatmonsterhack@gmail.com>
2026-02-06 21:07:38 +0700

    1

--- fe/src/pages/CreateWord.tsx
+++ fe/src/pages/CreateWord.tsx
@@ -121,6 +121,7 @@ const CreateWord: React.FC = () => {
                     ),
                     url('/bg.jpg')
                     `,
+                backgroundSize: "cover",
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat",
                 backgroundAttachment: "scroll" /* QUAN TRỌNG */,
@@ -136,7 +137,7 @@ const CreateWord: React.FC = () => {
                 <div className="absolute inset-0 bg-black/40 backdrop-blur"></div>
             </div> */}
 
-            <div className=" border-b border-slate-700 sticky top-0 z-20 flex-shrink-0">
+            <div className="border-b border-slate-700 sticky top-0 z-20 flex-shrink-0">
                 <div className="max-w-2xl  px-3 sm:px-4 py-2.5 sm:py-4 flex items-center gap-2 sm:gap-3">
                     <button
                         onClick={() =>
