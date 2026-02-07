92bef4951fd6da938ba97af18aec7ce4e7a6f283
TNMonster <nhatmonsterhack@gmail.com>
2026-02-07 11:53:48 +0700

    1

--- fe/src/pages/WordList.tsx
+++ fe/src/pages/WordList.tsx
@@ -176,7 +176,7 @@ const WordList: React.FC = () => {
     return (
         <>
             <div
-                className="min-h-screen bg-slate-50 flex flex-col"
+                className="bg-slate-50 flex flex-col"
                 style={{
                     height: "100dvh",
                     backgroundImage: `
